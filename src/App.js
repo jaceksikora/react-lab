38112b4ba28a697f175272fdfbe5dd02f6f0d2fa
Jacek Sikora <jack.sikor@gmail.com>
2025-12-14 10:59:32 +0100

    WallE use state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 
 function App() {
-    let title = 'Wall-E';
+    const [title, setTitle] = useState('Wall-E');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value);
     }
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+            <input type="text" value={title} onChange={handleChange}/>
         </div>
     );
 }
