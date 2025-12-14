7e12237a3ba311d4a5f44e453c9826822bef208c
Jacek Sikora <jack.sikor@gmail.com>
2025-12-14 12:10:24 +0100

    print titles list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-    const [title, setTitle] = useState('Wall-E');
-
-    let message;
-    if (title.length < 5) {
-        message = "Tytuł jest za krótki?";
-    } else if (title.length < 15) {
-        message = "Tytuł jest ekstra.";
-    } else {
-        message = "Tytuł jest za długi.";
-    }
-
-    function handleChange(event) {
-        setTitle(event.target.value);
-    }
-
+    const movies = [
+        {title: "Wall-E"},
+        {title: "Pulp Fiction"},
+        {title: "Matrix"},
+        {title: "1670"},
+    ];
+    
     return (
         <div>
             <h1>My favourite movies to watch</h1>
-            <h2>My favourite movie for today is {title}</h2>
-            <input type="text" value={title} onChange={handleChange} />
-            {title.length > 0 && <div>{message}</div>}
-            <button type="button" onClick={() => alert(title)}>
-                Pokaż tytuł filmu
-            </button>
+            <ul>
+                {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+            </ul>
         </div>
     );
 }
