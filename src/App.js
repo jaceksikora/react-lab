9d8cb6c2b34e1a4086b86d9ea23604ebc2c4db8c
Jacek Sikora <jack.sikor@gmail.com>
2025-12-14 11:09:55 +0100

    reactive title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
     const [title, setTitle] = useState('Wall-E');
+
+    let message;
+    if (title.length < 5) {
+        message = "Tytuł jest za krótki?";
+    } else if (title.length < 15) {
+        message = "Tytuł jest ekstra.";
+    } else {
+        message = "Tytuł jest za długi.";
+    }
 
     function handleChange(event) {
         setTitle(event.target.value);
@@ -11,7 +21,8 @@ function App() {
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" value={title} onChange={handleChange}/>
+            <input type="text" value={title} onChange={handleChange} />
+            {title.length > 0 && <div>{message}</div>}
         </div>
     );
 }
